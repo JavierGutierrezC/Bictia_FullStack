7e06306c434ce43588f23e793c9611a87d5dff65
Javier <javier.gutierrez.cruz@gmail.com>
2020-09-15 13:30:26 -0500

    multiplication table

---
+++ 2020-09-15_JSTabla_de_multiplicar/datos.js
@@ -0,0 +1,8 @@
+
+let result= 0;
+for (var x = 0; x < 11; x++) {
+    for (var y = 0; y < 10; y++){
+        result = (x * y);  
+        document.write("El resultado de  " + x + " x " + y + " = " + "<h1>" + result + "</h1>") 
+    } 
+}
